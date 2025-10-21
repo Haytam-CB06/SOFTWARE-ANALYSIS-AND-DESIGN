62afa0ec79c2cebb7a0d90932b9c1d2381bd2361
bestme1549 <damilola.fashola@final.edu.tr>
2025-10-21 19:36:36 +0300

    Update env.py

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,1 +1,8 @@
+import os
+from sqlalchemy import engine_from_config, pool
+from alembic import context
 
+config = context.config
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
