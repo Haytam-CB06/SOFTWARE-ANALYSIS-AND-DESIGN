b1c901459934447bd5e67de15074aa642ceb398c
Haytam-CB06 <haytam.charafi@final.edu.tr>
2026-02-24 10:14:57 +0200

    no snowfall

--- frontend/UPLAN/src/main.tsx
+++ frontend/UPLAN/src/main.tsx
@@ -3,12 +3,10 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
-  import Snowfall from "react-snowfall"
 
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
-      <Snowfall color="#fff" snowflakeCount={200} />
     </StrictMode>
   );
   
